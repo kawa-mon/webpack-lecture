df4d9c031d1c80ed92af795a12849b70db88f6dc
川島慎悟 <kawashingo1990@gmail.com>
2020-05-11 10:42:55 +0900

    webpack で css-loader を利用する記述

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,14 @@ module.exports = {
     filename: 'main.js',
     path: outputPath,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['css-loader'],
+      },
+    ],
+  },
   devServer: {
     open: true, // コマンド実行時にブラウザを開くかどうかのオプション
     contentBase: outputPath, //　サーバ立ち上げ時に開くディレクトリ
